b26bbd5a4cce2fb0ff599b0ee621d3a52f18a49d
Sulman <sulmanqureshi1@gmail.com>
2017-04-25 19:16:10 -0400

    Successfully have all training files and testing files loading in their own lists

--- src/main/java/Library/neuralnetwork/NeuralNetwork.java
+++ src/main/java/Library/neuralnetwork/NeuralNetwork.java
@@ -2,10 +2,14 @@ package Library.neuralnetwork;
 
 import Library.layer.LayerCreation;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
 
 
 public class NeuralNetwork {
@@ -13,7 +17,8 @@ public class NeuralNetwork {
 	
 	public NeuralNetwork(){
 		
-		loadTrainingFiles();
+		//loadTrainingFiles(",");
+		loadTestingFiles(",");
 		// neuron(sizeofInput, numberofHidden)
 		//INDArray hiddenLayer = getHiddenLayer(training_files.size(1), 2);
 		//System.out.println(hiddenLayer);
@@ -39,29 +44,66 @@ public class NeuralNetwork {
 	}
 	
 	/**
-	 * 
-	 * @param fileName
-	 * @param delimiter
-	 * @return
+	 * This method returns a list of type INDArray containing all the lines
+	 * of every training file.
+	 * @param delimeter - usually a comma that would be removed from the currentFile.
 	 */
-    public INDArray loadTrainingFiles(){
-
+    public List<INDArray> loadTrainingFiles(String delimeter){
 		List<String> allTrainingFiles = new ArrayList<String>();
 		allTrainingFiles = loadAllFiles(dir + "//a1digits");
+		List<INDArray> trainingData = new ArrayList<INDArray>();
 		Iterator<String> iter = allTrainingFiles.iterator();
 		
-        while(iter.hasNext()){
-            if(iter.next().contains("test"))
-                iter.remove();
+		//iterator used to iterate over the trainingData of files in allTrainingFiles and remove all test currentFile names.
+		while(iter.hasNext()){
+			if(iter.next().contains("test"))
+					iter.remove();
 		}
-		System.out.println(allTrainingFiles);
-//		try {
-//			training_files = Nd4j.readNumpy(fileName, delimiter);
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		return null;
+		
+		INDArray currentFile = null;
+		for(int i = 0; i < allTrainingFiles.size(); i++){
+			try {
+				currentFile = Nd4j.readNumpy(allTrainingFiles.get(i), delimeter);
+				
+			} catch (IOException ex) {
+				Logger.getLogger(NeuralNetwork.class.getName()).log(Level.SEVERE, null, ex);
+			}	
+			for(int j = 0; j < currentFile.size(0); j++){
+				trainingData.add(currentFile.getRow(j));
+			}
+		}
+		return trainingData;
+	}
+	/**
+	 * This method returns a list of type INDArray containing all the lines
+	 * of every testing file.
+	 * @param delimeter - usually a comma that would be removed from the currentFile.
+	 */
+    public List<INDArray> loadTestingFiles(String delimeter){
+		List<String> allTestingFiles = new ArrayList<String>();
+		allTestingFiles = loadAllFiles(dir + "//a1digits");
+		List<INDArray> testData = new ArrayList<INDArray>();
+		Iterator<String> iter = allTestingFiles.iterator();
+		
+		//iterator used to iterate over the testData of files in allTestingFiles and remove all training currentFile names.
+		while(iter.hasNext()){
+			if(iter.next().contains("training"))
+					iter.remove();
+		}
+		
+		INDArray currentFile = null;
+		for(int i = 0; i < allTestingFiles.size(); i++){
+			try {
+				currentFile = Nd4j.readNumpy(allTestingFiles.get(i), delimeter);
+				
+			} catch (IOException ex) {
+				Logger.getLogger(NeuralNetwork.class.getName()).log(Level.SEVERE, null, ex);
+			}	
+			for(int j = 0; j < currentFile.size(0); j++){
+				testData.add(currentFile.getRow(j));
+			}
+		}
+		return testData;
 	}
 	
     /**
