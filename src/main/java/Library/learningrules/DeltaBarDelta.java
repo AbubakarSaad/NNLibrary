1987bf74d2f28977808ab6c069874cd3c684719e
Sulman <sulmanqureshi1@gmail.com>
2017-04-27 00:40:14 -0400

    Created Javadocs and they look cleannn

--- src/main/java/Library/learningrules/DeltaBarDelta.java
+++ src/main/java/Library/learningrules/DeltaBarDelta.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Library.learningrules;
 
 /**
- *
- * @author Sulman633
+ *This class is the main implementation of delta-bar-delta, it works in 
+ * conjunction with the gradient collector class.
+ * @author Sulman and Abubakar
  */
 public class DeltaBarDelta {
 	
